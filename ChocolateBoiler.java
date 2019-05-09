0e7a16da4ccaae37ed4deb0ebaa05b24993c0005
Camila Fernandez <camila.fernandez@utec.edu.pe>
2019-05-09 12:52:41 -0500

    I tried

--- ChocolateBoiler.java
+++ ChocolateBoiler.java
@@ -1,8 +1,21 @@
-public class ChocolateBoiler {
+public class ChocolateBoiler{
     private boolean empty;
     private boolean boiled;
 
-    public ChocolateBoiler() {
+    private static ChocolateBoiler instance;
+
+    public static ChocolateBoiler getInstance() {
+        if (instance == null){
+            instance = new ChocolateBoiler();
+            System.out.println("Creado");
+        }
+        else{
+            System.out.println("No creado");
+        }
+        return instance;
+    }
+
+    private ChocolateBoiler() {
         empty = true;
         boiled = false;
     }
@@ -34,5 +47,39 @@ public class ChocolateBoiler {
 
     public boolean isBoiled(){
         return boiled;
+    }
+}
+
+public class CreateThread extends Thread{
+    @Override
+    public void run(){
+        try {
+            ChocolateBoiler CB = ChocolateBoiler.getInstance();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted");
+
+        }
+        System.out.println("exiting.");
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+
+        CreateThread r = new CreateThread();
+        r.start();
+        CreateThread r2 = new CreateThread();
+        r2.start();
+        CreateThread r3 = new CreateThread();
+        r3.start();
+
+        try{
+            r.join();
+            r2.join();
+            r3.join();
+        }
+
     }
 }
